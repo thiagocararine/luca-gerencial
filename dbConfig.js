5b1326a113a07d9cfead8dbf13349e9f7c5c2efc
Thiago Cararine <thiagocararine@live.com>
2025-07-30 11:10:03 -0300

    Ajuste Final Despesa.js 06

--- dbConfig.js
+++ dbConfig.js
@@ -7,7 +7,8 @@ const dbConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    charset: 'utf8mb4'
 };
 
 module.exports = dbConfig;
