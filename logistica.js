01c129547ccd491ae31cf609bdd3f01f769af797
Thiago Cararine <thiagocararine@live.com>
2025-08-15 12:28:36 -0300

    Ajuste Logistica

--- logistica.js
+++ logistica.js
@@ -789,14 +789,14 @@ async function openVehicleModal(vehicle = null) {
             document.getElementById('vehicle-filial').value = vehicle.id_filial || '';
             document.getElementById('vehicle-status').value = vehicle.status || 'Ativo';
             
-            //const seguroCheckbox = document.getElementById('vehicle-seguro');
-            //if (seguroCheckbox) {
-            //    seguroCheckbox.checked = !!vehicle.seguro;
-            //}
-            //const rastreadorCheckbox = document.getElementById('vehicle-rastreador');
-            //if (rastreadorCheckbox) {
-            //    rastreadorCheckbox.checked = !!vehicle.rastreador;
-            //}
+            const seguroCheckbox = document.getElementById('vehicle-seguro');
+            if (seguroCheckbox) {
+                seguroCheckbox.checked = !!vehicle.seguro;
+            }
+            const rastreadorCheckbox = document.getElementById('vehicle-rastreador');
+            if (rastreadorCheckbox) {
+                rastreadorCheckbox.checked = !!vehicle.rastreador;
+            }
 
             document.getElementById('vehicle-tipo-combustivel').value = vehicle.tipo_combustivel || '';
             const hasPlaca = vehicle.placa && vehicle.placa.toUpperCase() !== 'SEM PLACA';
@@ -811,7 +811,7 @@ async function openVehicleModal(vehicle = null) {
         modal.classList.remove('hidden');
         feather.replace();
     } catch(error) {
-        alert("Erro ao abrir o modal do veículo.");
+        alert("Erro ao abrir o modal do veículo Logistica.js.");
     } finally {
         hideLoader();
     }
