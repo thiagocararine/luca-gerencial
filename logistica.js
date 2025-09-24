05b6d3d5137ed074f248b0cc2b328b94004a6e49
Thiago Cararine <thiagocararine@live.com>
2025-09-24 10:26:38 -0300

    ultimos ajustes produtos

--- logistica.js
+++ logistica.js
@@ -1432,7 +1432,7 @@ async function handleVehicleCostFormSubmit(event) {
         classificacao_custo: document.getElementById('vehicle-cost-classification').value,
         descricao: document.getElementById('vehicle-cost-description').value,
         id_fornecedor: document.getElementById('vehicle-cost-fornecedor-id').value,
-        numero_nf: document.getElementById('vehicle-cost-nf').value,
+        numero_nf: document.getElementById('vehicle-cost-nf').value || '0' // Pega a NF ou envia '0' se estiver vazia
         item_servico: document.getElementById('vehicle-cost-item-servico').value,
         odometro_manutencao: document.getElementById('vehicle-cost-odometer').value
     };
@@ -1499,7 +1499,7 @@ async function handleFleetCostFormSubmit(event) {
         custo: document.getElementById('fleet-cost-value').value,
         data_custo: document.getElementById('fleet-cost-date').value,
         id_fornecedor: document.getElementById('fleet-cost-fornecedor-id').value,
-        numero_nf: document.getElementById('fleet-cost-nf').value,
+        numero_nf: document.getElementById('fleet-cost-nf').value || '0' // Pega a NF ou envia '0' se estiver vazia
         filiais_rateio: selectedFiliais
     };
     if (!costData.id_fornecedor) {
